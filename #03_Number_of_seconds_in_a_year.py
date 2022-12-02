5b3a5fd864ec3b99370344e40badedfa96743b53
Akbar <87269463+akbartangir99@users.noreply.github.com>
2022-12-02 17:54:56 +0500

    Create #03_Number_of_seconds_in_a_year.py

---
+++ #03_Number_of_seconds_in_a_year.py
@@ -0,0 +1,5 @@
+# Write a Python program displaying the number of seconds in 365 days.
+# You'll need the print function to display the result.
+# You'll need the * operator for the multiplications.
+print(((60 * 60) * 24) * 365)
+
